dbad6be4cb869bc467a0c4a7fb2766fa94ddeea5
Kari Gilbertson <kari.a.gilbertson@gmail.com>
2018-06-12 21:36:12 -0400

    Test moving js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var config = {
-	client_id: "c6351bf1ec32f93010b1d38ceeaea6e138d8e7d2",
-	authorization: "https://launchpad.37signals.com/authorization/new?type=web_server",
-	redirect_uri: "http://gilberk1.github.io/BC3/success.html",
-	response_type: 'code',
-	client_secret: "de2b736120821d2ac3436b0f1a46f635fc4236b9",
-	token: "https://launchpad.37signals.com/authorization/token"
-};
+// var config = {
+// 	client_id: "c6351bf1ec32f93010b1d38ceeaea6e138d8e7d2",
+// 	authorization: "https://launchpad.37signals.com/authorization/new?type=web_server",
+// 	redirect_uri: "http://gilberk1.github.io/BC3/success.html",
+// 	response_type: 'code',
+// 	client_secret: "de2b736120821d2ac3436b0f1a46f635fc4236b9",
+// 	token: "https://launchpad.37signals.com/authorization/token"
+// };
 
-var j = new jso.JSO(config);
+// var j = new jso.JSO(config);
 
-j.callback();
+// j.callback();
 
-var opts = {
+// var opts = {
 
-};
+// };
 
-j.getToken(opts).then((token) => {
-    console.log("I got the token: ", token)
- });
+// j.getToken(opts).then((token) => {
+//     console.log("I got the token: ", token)
+//  });
